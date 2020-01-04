037e849a61b722c43eb92381532df9294906ccf7
malik192 <malikghern@gmail.com>
2020-01-05 00:29:24 +0100

    EF core Code first model

---
+++ VNext.BienEtreAuTravail.DAL/Context/WorkContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VNext.BienEtreAuTravail.DAL.Models.Database;
+
+namespace VNext.BienEtreAuTravail.DAL.Context
+{
+    class WorkContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Commentaire> Commentaires { get; set; }
+        public DbSet<CommentaireEmp> CommentaireEmps{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=tcp:mg-sql-server.database.windows.net,1433;Initial Catalog=vnext;Persist Security Info=False;User ID=ad-min;Password=Vnext#14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CommentaireEmp>().HasKey(sc => new { sc.IdEmployee, sc.IdCommentaire });
+        }
+    }
+}
---
+++ VNext.BienEtreAuTravail.DAL/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace VNext.BienEtreAuTravail.DAL.Models.Database
+{
+    public class User
+    {
+       
+        public int Id { get; set; }        
+        public String Name { get; set; }
+    }
+    public class Employee 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdEmployee { get; set; }
+        public string Pseudo { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        IList<CommentaireEmp> CommentaireEmployee { get; set; }
+        public Employee(string pseudo)
+        {
+            this.Pseudo = pseudo;
+        }
+    }
+    public class Commentaire 
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCommentaire { get; set; }
+        public string Description { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        IList<CommentaireEmp> CommentaireEmployee { get; set; }
+    }
+
+    internal class CommentaireEmp
+    {
+        public int IdEmployee { get; set; }
+        public Employee Employee { get; set; }
+        public int IdCommentaire { get; set; }
+        public Commentaire Commentaire { get; set; }
+    }
+}
